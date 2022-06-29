c3a0e42b29810130799202f8560bc92476daac35
Kimberly <kimberlysoguilon14@gmail.com>
2022-06-29 10:57:33 +0800

    update certification

--- page/component/certification.php
+++ page/component/certification.php
@@ -1,13 +1,16 @@
 <div id="div-carousel-certification" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-inner">
+        <div class="carousel-item div-carousel-item">
+            <img src="../asset/img/placeholder/n.webp" class="d-block w-100" alt="...">
+            <h4 class="txt-carousel-desc">The Association of Home Appliance Manufacturers (AHAM) certifies that the Rainbow is a proven air cleaner designed to reduce air pollutants that contribute to poor indoor air quality<h4>
+        </div>
+        <div class="carousel-item div-carousel-item">
+            <img src="../asset/img/placeholder/p.jpg" class="d-block w-100" alt="...">
+            <h4 class="txt-carousel-desc">The Rainbow is certified asthma & allergy friendly™ by the Asthma and Allergy Foundation of America<h4>
+        </div>
         <div class="carousel-item active div-carousel-item">
-            <img src="/greendenpeak/greendenpeak/asset/img/placeholder/m.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item div-carousel-item">
-            <img src="/greendenpeak/greendenpeak/asset/img/placeholder/n.webp" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item div-carousel-item">
-            <img src="/greendenpeak/greendenpeak/asset/img/placeholder/p.jpg" class="d-block w-100" alt="...">
+            <img src="../asset/img/placeholder/m.jpg" class="d-block w-100" alt="...">
+            <h4 class="txt-carousel-desc">The Rainbow has met the Carpet Industry Standard for all three elements: soil removal, dust containment and carpet appearance<h4>
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#div-carousel-certification" data-bs-slide="prev">
