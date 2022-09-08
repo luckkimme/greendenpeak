bdbb42eb5f15f873185a390eb21e2622d3a8bcf7
Kimberly <kimberlysoguilon14@gmail.com>
2022-09-08 16:59:27 +0800

    fixed bug

--- page/component/product-accessories.php
+++ page/component/product-accessories.php
@@ -4,6 +4,6 @@
     <h3 class="fw-bold lh-1 text-center mb-3 py-3"><?php echo $product_accessory_title['product_accessory_title']; ?></h3>
     <p class="text-center mx-5"><?php echo $product_accessory_title['product_accessory_subtitle']; ?></p>    
     <div class="row row-cols-2 row-cols-md-3 g-4 px-3 py-3">
-        <?php echo display_product_accessory($product_accessory, $product_accessory_desc, $product_accessory_desc_list); ?>
+        <?php echo display_product_accessory($product_accessory); ?>
     </div>
 </div>
--- page/component/product-other-info.php
+++ page/component/product-other-info.php
@@ -2,7 +2,7 @@
     <!-- <div class="b-example-divider-v2"></div> -->
     <?php 
         if($product_other_title) {
-            echo display_product_other_info_per_keyword($product_other_title, $product_other_info, $product_other_info_desc, $product_other_info_desc_list);
+            echo display_product_other_info_per_keyword($product_other_title, $product_other_info);
         }
     ?>
 </div>
