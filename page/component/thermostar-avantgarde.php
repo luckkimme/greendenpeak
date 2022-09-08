5280142b392bce97201a33be7eab2b6a084725b2
Kimberly <kimberlysoguilon14@gmail.com>
2022-09-08 13:53:23 +0800

    deleted unnecessary pages

--- page/component/thermostar-avantgarde.php
+++
@@ -1,4 +0,0 @@
-<h1>Thermostar Avant Garde S4</h1>
-<div>
-    <img src="../asset/img/placeholder/thermostar_media/avantgardeS4.png" class="d-block" alt="...">
-</div>
--- page/component/thermostar-professional.php
+++
@@ -1,4 +0,0 @@
-<h1>Thermostar Professional S4</h1>
-<div>
-<img src="../asset/img/placeholder/thermostar_media/professionalS4.png" class="d-block" alt="...">
-</div>
